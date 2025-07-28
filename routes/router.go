41196955384c66c7bbeaf202be4256d2db8ef758
nickolss <nickolasmaiaraujo@gmail.com>
2025-07-28 07:45:58 -0300

    feat: added router

---
+++ routes/router.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nickolss/financial_api/handler"
+)
+
+func InitRouter() {
+	router := gin.Default()
+	// Auth routes
+	router.POST("/register", handler.RegisterUser)
+	router.POST("/login", handler.Login)
+	router.GET("/getUser", handler.GetUser)
+
+	// Operation routes
+	router.POST("/financialEvent", handler.CreateFinancialEvent)
+	router.GET("/financialEvent", handler.GetAllFinancialEvents)
+	router.GET("/financialEvent/:id", handler.GetEventById)
+	router.PUT("/financialEvent/:id", handler.UpdateFinancialEvent)
+	router.DELETE("/financialEvent/:id", handler.DeleteEventById)
+
+	// Category routes
+	router.GET("/category", handler.GetAllCategories)
+	router.POST("/category", handler.AddCategory)
+	router.DELETE("/category/:id", handler.DeleteCategoryById)
+
+	router.Run(":9999")
+}
